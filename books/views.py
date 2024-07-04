65b3da3d24b86ed7544442667dd09fd3399d7f5f
vladislav918 <lvlal5759@gmail.com>
2024-07-05 03:22:51 +0700

    Fix: Class ReadListModelViewSet

--- books/views.py
+++ books/views.py
@@ -1,9 +1,13 @@
 from rest_framework.response import Response
-from rest_framework import viewsets, status, permissions
+from rest_framework import viewsets, status, permissions, mixins
+from rest_framework.decorators import action
+
+from django.shortcuts import get_object_or_404
 
 from .filters import ReadBookListFilter
 from .models import Book, ReadList
 from .serializers import BookSerializer, ReadListSerializer
+from rest_framework.views import APIView
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -11,52 +15,44 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
 
-class ReadListModelViewSet(viewsets.ModelViewSet):
+class ReadListModelViewSet(mixins.CreateModelMixin,
+                           mixins.DestroyModelMixin,
+                           mixins.ListModelMixin,
+                           viewsets.GenericViewSet):
     """
     Класс для отображения, добавления и удаления книг в список "Прочитанных"
     """
-    queryset = ReadList.objects.all()
     serializer_class = ReadListSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
     def get_queryset(self):
         return ReadList.objects.filter(user=self.request.user)
 
 
     def create(self, request):
-        book_id = request.data.get('book')
-        if not book_id:
-            return Response(
-                {'error': 'Не указан ID книги'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+        serializer = ReadListSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        try:
-            book = Book.objects.get(pk=book_id)
-        except Book.DoesNotExist:
-            return Response(
-                {'error': 'Книга не найдена'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        if ReadList.objects.filter(
+                user=request.user,
+                book=serializer.validated_data['book']
+            ).exists():
 
-        if ReadList.objects.filter(user=request.user, book=book).exists():
             return Response(
                 {'error': 'Эта книга уже добавлена в список "Прочитанное"'},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        ReadList.objects.create(user=request.user, book=book)
-        return Response({'message': 'Книга добавлена в список "Прочитанное"'})
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-    def destroy(self,request, pk=None):
-        try:
-            read_book = ReadList.objects.get(user=request.user, book_id=pk)
-        except ReadList.DoesNotExist:
-            return Response(
-                {'error': 'Книга не найдена в списке "Прочитанное"'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+    def destroy(self, request, pk=None):
+        read_book = ReadList.objects.filter(user=request.user, book_id=pk)
 
-        read_book.delete()
-        return Response({'message': 'Книга удалена из списка "Прочитанное"'})
+        if read_book.exists():
+            read_book.delete()
+            return Response({'message': 'Книга удалена из списка "Прочитанное"'})
+
+        return Response({'error': 'Книга не найдена'})
