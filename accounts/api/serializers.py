0088ab65bf7ebfd6d182581ae9b299ff9a5c4a24
vladislav918 <lvlal5759@gmail.com>
2024-10-05 00:38:46 +0700

    change serializers

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,6 +1,26 @@
+import re
+
+from django.core.validators import MinLengthValidator
+
 from rest_framework import serializers
 
 from ..domain.models import User
+
+
+class PasswordValidator:
+    def __call__(self, value):
+        MinLengthValidator(8)(value)
+        
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну заглавную букву.")
+        
+        if not re.search(r'[0-9]', value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну цифру.")
+        
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы один специальный символ.")
+        
+        return value
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,31 +29,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email')
 
 
-class UserRegisterationSerializer(serializers.ModelSerializer):
+class UserRegisterationSerializer(UserSerializer):
     confirm_password = serializers.CharField(
         style={'input_type': 'password'},
-        write_only=True
-    )
-    password = serializers.RegexField(
-        regex=r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$',
         write_only=True,
-        error_messages={
-            'invalid': ('Пароль должен быть длиной не менее 8 символов с хотя бы одной заглавной буквой и символом')}
+        required=True,
+    )
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        required=True,
+        validators=[PasswordValidator()]
     )
 
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password', 'confirm_password')
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ('password', 'confirm_password')
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
         return data
 
 
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
 
 
 class ResetPasswordRequestSerializer(serializers.Serializer):
@@ -41,25 +61,29 @@ class ResetPasswordRequestSerializer(serializers.Serializer):
 
 
 class ResetPasswordSerializer(serializers.Serializer):
-    new_password = serializers.RegexField(
-        regex=r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$',
+    new_password = serializers.CharField(
+        style={'input_type': 'password'},
         write_only=True,
-        error_messages={
-            'invalid': ('Пароль должен быть длиной не менее 8 символов с хотя бы одной заглавной буквой и символом')}
+        required=True,
+        validators=[PasswordValidator()]
     )
     confirmed_password = serializers.CharField(write_only=True, required=True)
 
     def validate(self, data):
         if data['new_password'] != data['confirmed_password']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
         return data
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(style={'input_type': 'password'})
-    new_password = serializers.RegexField(
-        regex=r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$',
+    old_password = serializers.CharField(
         write_only=True,
-        error_messages={
-            'invalid': ('Пароль должен быть длиной не менее 8 символов с хотя бы одной заглавной буквой и символом')}
+        required=True,
+        style={'input_type': 'password'}
+    )
+    new_password = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        required=True,
+        validators=[PasswordValidator()]
     )
